5188b2464ad7d0fe451bfe34282d19a593bf8585
Sirb Alin Cristian <52006986+sirbalin26@users.noreply.github.com>
2019-11-22 17:23:21 +0200

    Add files via upload

---
+++ run.CryptoDeco.js
@@ -0,0 +1,29 @@
+// Creat de Sirb Alin Cristian
+
+function encrypt(_) {
+	var __ = "";
+	[..._].forEach(___ => __ += String.fromCharCode(~___.charCodeAt(0)));
+	show(__);
+}
+
+function func() {
+	var inp = document.getElementById("in");
+	var tinp = inp.value;
+	if (tinp === "") {
+		alert("You think I am a fool?!");
+	} else {
+		encrypt(tinp);
+	}
+}
+
+function show(outp) {
+	var en = document.getElementById("en");
+	var res = document.getElementById("result");
+	en.innerHTML = outp;
+	res.style.transform = "scale(1)";
+}
+
+function hide() {
+	var res = document.getElementById("result");
+	res.style.transform = "scale(0)"
+}
